0e3039c7439e6847cdb3cb0c67c62aa4bdf77106
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-21 17:06:37 -0400

    added validation tests for flights and association tests for airports

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -3,20 +3,34 @@ require 'test_helper'
 class AirportTest < ActiveSupport::TestCase
 
   def setup
-    @airport = airports(:one)
+    @departure_airport = airports(:one)
+    @arrival_airport = airports(:two)
+    @flight_to_cancel = flights(:two)
   end
 
   test "should be valid" do
-    assert @airport.valid?
+    assert @departure_airport.valid?
   end
 
   test "name should be present" do
-    @airport.name = nil
-    assert_not @airport.valid?
+    @departure_airport.name = nil
+    assert_not @departure_airport.valid?
   end
 
   test "abbreviation should be present" do
-    @airport.abbreviation = nil
-    assert_not @airport.valid?
+    @departure_airport.abbreviation = nil
+    assert_not @departure_airport.valid?
+  end
+
+  test "airport has departing flights associated" do
+    assert_difference '@departure_airport.departing_flights.count', -1 do
+      @flight_to_cancel.destroy
+    end
+  end
+
+  test "airport has arriving flights associated" do
+    assert_difference '@arrival_airport.arriving_flights.count', -1 do
+      @flight_to_cancel.destroy
+    end
   end
 end
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -7,7 +7,36 @@ class FlightTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-    debugger
     assert @flight.valid?
+  end
+
+  test "needs a departure airport" do
+    @flight.departure_airport = nil
+    assert_not @flight.valid?
+  end
+
+  test "needs an arrival airport" do
+    @flight.arrival_airport = nil
+    assert_not @flight.valid?
+  end
+
+  test "needs a flight number" do
+    @flight.flight_number = nil
+    assert_not @flight.valid?
+  end
+
+  test "needs a price" do
+    @flight.price = nil
+    assert_not @flight.valid?
+  end
+
+  test "needs a departure date" do
+    @flight.departure_date = nil
+    assert_not @flight.valid?
+  end
+
+  test "needs an arrival date" do
+    @flight.arrival_date = nil
+    assert_not @flight.valid?
   end
 end
