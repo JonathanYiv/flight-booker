1378a4bf856f8120e5972ae8ae7542bf498c298c
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-22 02:33:43 -0400

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Creates 10 Airports with random Cities / Codes.
+
+10.times do |n|
+  name = Faker::Address.city
+  abbreviation = Faker::Address.country_code_long
+  Airport.create(name:         name,
+                 abbreviation: abbreviation)
+end
+
+airports = Airport.all
+
+10.times do |n|
+  departure_airport = airports.sample
+  arrival_airport = airports.sample
+  until departure_airport != arrival_airport
+    arrival_airport = airports.sample
+  end
+  flight_number = Faker::Number.number(3)
+  price = Faker::Number.decimal(2)
+  departure_date = Faker::Time.between(DateTime.now, 7.days.from_now)
+  arrival_date = Faker::Time.between(departure_date, departure_date + 17.hours)
+  Flight.create(departure_airport: departure_airport,
+                arrival_airport:   arrival_airport,
+                flight_number:     flight_number,
+                price:             price,
+                departure_date:    departure_date,
+                arrival_date:      arrival_date)
+end
