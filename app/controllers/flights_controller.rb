18f21254da5f2eac387ae26d2a52a9d9126a12bc
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-25 15:22:29 -0400

    finished search function

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,25 @@ class FlightsController < ApplicationController
 
   def search
     @flights = Flight.all
+    @passengers = params[:passengers]
+    @passengers ||= 1
+
+    if params[:flight]
+      if params[:flight][:departure_date]
+        departure_date = DateTime.strptime(params[:flight][:departure_date], '%D')
+        departure_search_range = departure_date.midnight..(departure_date.midnight + 1.day)
+        @flights = @flights.where(departure_date: departure_search_range)
+      end
+
+      if params[:flight][:departure_airport]
+        @flights = @flights.where("departure_airport_id = ?", params[:flight][:departure_airport])
+      end
+
+      if params[:flight][:arrival_airport]
+        @flights = @flights.where("arrival_airport_id = ?", params[:flight][:arrival_airport])
+      end
+    end
+
     render 'static_pages/home'
   end
 end
